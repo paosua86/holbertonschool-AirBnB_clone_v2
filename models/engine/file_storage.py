3b697269f4ba7f7a305f3347411cafa7e280ac40
paosua86 <paolasuarezb1986@gmail.com>
2022-12-01 14:33:17 -0500

    file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,3 +55,14 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
