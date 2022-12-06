ba6c58d6d9a95b294f34d7970751a8596d697429
paosua86 <paolasuarezb1986@gmail.com>
2022-12-06 12:16:27 -0500

    7 working

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,27 +2,28 @@
 """First flask module for HBNB"""
 
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_close(self):
-    "Closses sqlalchemy session"
+def teardown_app_close(self):
+    "Closses session"
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def states_list():
     """Returns a HTML with states list"""
     state_dict = storage.all(State)
-    ret_list = []
+    list = []
     for state in state_dict.values():
-        ret_list.append(state)
-    return render_template('7-states_list.html', st_list=ret_list)
+        list.append(state)
+    return render_template('7-states_list.html', sorted_list=list)
 
 
 if __name__ == "__main__":
+    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port="5000")
